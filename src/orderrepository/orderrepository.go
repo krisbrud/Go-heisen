5f5c0a55b18b03fe4afa138dcb6412ce6ab09463
Kristian <krbrudeli@gmail.com>
2020-02-26 15:44:07 +0100

    OrderRepository refactored

--- src/orderrepository/orderrepository.go
+++ src/orderrepository/orderrepository.go
@@ -2,27 +2,25 @@ package orderrepository
 
 import (
 	"Go-heisen/src/order"
+	"fmt"
 )
 
 // ReadRequest serves as a request to read an order from OrderRepository
 type ReadRequest struct {
 	OrderID    string
 	ResponseCh chan order.Order
-	ErrorCh    chan error
+}
+
+// MakeReadRequest returns a ReadRequest with a new response channel
+func MakeReadRequest(OrderID string) ReadRequest {
+	return ReadRequest{OrderID, make(chan order.Order)}
 }
 
 // WriteRequest makes it possible for other modules to write to OrderRepository
 type WriteRequest struct {
 	OrderToWrite order.Order
-	ErrorCh      chan error
+	successCh    chan bool
 }
-
-// The InvalidRepoRequestError is returned on ErrorCh in ReadRequest or WriteRequest if something is wrong.
-type InvalidRepoRequestError struct {
-	why string
-}
-
-func (e InvalidRepoRequestError) Error() string { return e.why }
 
 // OrderRepository is the single source of truth of all known orders in all nodes.
 func OrderRepository(
@@ -36,33 +34,35 @@ func OrderRepository(
 		select {
 		case readReq := <-readSingleRequests:
 			storedOrder, ok := allOrders[readReq.OrderID]
-
-			if ok {
-				readReq.ErrorCh <- nil
-			} else {
+			if !ok {
 				// Order does not exist, inform Reader by sending invalid order back
 				storedOrder = order.NewInvalidOrder()
-				readReq.ErrorCh <- InvalidRepoRequestError{"Order with ID: " + readReq.OrderID + " already exists."}
 			}
 
-			readReq.ResponseCh <- storedOrder
+			readReq.ResponseCh <- storedOrder // Send order back to requester
 			close(readReq.ResponseCh)
-			close(readReq.ErrorCh)
 
 		case readReq := <-readAllActiveRequests:
-			// Read back all orders on the request
+			// Read back all orders on the request and close channel afterwards
 			for _, storedOrder := range allOrders {
 				if !storedOrder.Completed {
-
+					if storedOrder.IsValid() {
+						readReq.ResponseCh <- storedOrder
+					} else {
+						// Invalid Order in OrderRepository for some reason. Restart.
+						// TODO restart mechanics
+						fmt.Println("Invalid order in repository!")
+					}
 				}
 			}
+			close(readReq.ResponseCh)
 
 		case writeReq := <-writeRequests:
 			if writeReq.OrderToWrite.IsValid() {
 				allOrders[writeReq.OrderToWrite.OrderID] = writeReq.OrderToWrite
-				writeReq.ErrorCh <- nil
+				go func() { writeReq.successCh <- true }() // Don't wait for successCh to be read
 			} else {
-				writeReq.ErrorCh <- InvalidRepoRequestError{"Trying to write invalid order."}
+				go func() { writeReq.successCh <- false }()
 			}
 		}
 	}
