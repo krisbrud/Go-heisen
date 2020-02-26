20490cadecef963418f26ec830cdc9163f3f8410
Helga Sangolt <helgasangolt@dhcp-10-22-68-139.wlan.ntnu.no>
2020-02-26 16:50:07 +0100

    en grei start

--- src/controller/controller.go
+++ src/controller/controller.go
@@ -1,10 +1,51 @@
 package controller
 
-func Controller() {
+import (
+	
+)
+
+
+func Controller(
+	toButtonPushHandler chan order.Order, 
+	toArrivedFloorHandler chan elevatorstate.ElevatorState, 
+	toDelegator chan elevatorstate.ElevatorState, 
+	readState chan elevatorstate.ElevatorState,
+	readQueue chan order.Order
+) {
+	
 	// TODO Setup/init code here
 	// TODO declare channels here
+	
+	var state elevatorstate.ElevatorState  
+	var floor int
+	var destination int 
 
 	for {
-		select {}
+		select {
+			
+		case buttonPushed := <- drv_buttons:
+			go func() { toButtonPushHandler <- buttonPushed }() // må sende dette til button pushed handler 
+
+		case elevatorStateChanged := <-readState:
+			if elevatorStateChanged != state{
+				go func() { toDelegator <- elevatorStateChanged }() // må lese elevator state og sende den til delegatoren 
+				state := elevatorStateChanged
+			}
+			
+		case arrivedAtFloor := <- drv_floors: 
+			if arrivedFloor == destination{
+				elevio.SetMotorDirection(elevio.MD_Stop)
+				go func() { toArrivedFloorHandler := <- drv_floors }() // må sende beskjed til arrived floor handler 
+			}
+			
+		case orderToExecute := <- reaadQueue:
+			floor = elevio.GetFloor()
+			destination = orderToExecute.Floor
+			if floor < destination {
+				elevio.SetMotorDirection(elevio.MD_Up)
+			} else if floor > destination {
+				elevio.SetMotorDirection(elevio.MD_Down)
+			}
+		}
 	}
 }
