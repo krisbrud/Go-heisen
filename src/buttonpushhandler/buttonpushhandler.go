d5c3d5f732ba36bb79d7a30663c501d78902f140
Helga Sangolt <helgasangolt@Helgas-MacBook-Air.local>
2020-03-01 16:39:16 +0100

    har ikke testet implementert kode

--- src/buttonpushhandler/buttonpushhandler.go
+++ src/buttonpushhandler/buttonpushhandler.go
@@ -8,11 +8,32 @@ import (
 // ButtonPushHandler checks if an order alre
 func ButtonPushHandler(
 	buttonPushOrders chan order.Order,
-	buttonRepoReads chan order.Order,
-	repoReadRequests chan readrequest.ReadRequest,
+	readAllActiveRequests chan orderrepository.ReadRequest,
 	toDelegatorChan chan order.Order,
 ) {
+	var activeOrders order.Order; 
 	for {
-		select {}
+		select {
+		case readButtonPush := <- buttonPushOrders:
+			// Lag readreq. for alle aktive ordre
+			// Vent på resultat
+			// Sjekk at ingen aktive ordre er like
+			// 		send i så fall videre
+			activeOrders := <- readAllActiveRequests;
+			var orderExist = false;
+			if readButtonPush.IsValid() {
+				for order in activeOrders{
+					if order == readButtonPush{
+						orderExist = true; 
+					}
+				
+				}
+				if !orderExist{
+					toDelegatorChan <- readButtonPush
+				}
+			}
+		}
 	}
 }
+
+
