e3788a373b64dcf37a7e849a66bf2da65f2377bd
Helga Sangolt <helgasangolt@Helgas-MacBook-Air.local>
2020-03-01 16:43:03 +0100

    deler av light manager lagt til

--- src/controller/controller.go
+++ src/controller/controller.go
@@ -35,7 +35,7 @@ func Controller(
 		case arrivedAtFloor := <- drv_floors: 
 			if arrivedFloor == destination{
 				elevio.SetMotorDirection(elevio.MD_Stop)
-				go func() { toArrivedFloorHandler := <- drv_floors }() // må sende beskjed til arrived floor handler 
+				go func() { toArrivedFloorHandler <- drv_floors }() // må sende beskjed til arrived floor handler 
 			}
 			
 		case orderToExecute := <- reaadQueue:
@@ -47,5 +47,42 @@ func Controller(
 				elevio.SetMotorDirection(elevio.MD_Down)
 			}
 		}
+	}
+}
+
+
+func lightManager(
+	incomingOrders chan order.Order,
+	toElevatorNetwork chan order.Order
+) {
+	
+	for {
+		select {
+		case handleIncomingOrder := <- incomingOrders: 
+			go func() {
+				if handleIncomingOrder.Completed == false {
+					if handleIncomingOrder.Class == CAB { //legg inn at dette kun skal skje for den heisen det gjelder 
+						SetButtonLamp(2, handleIncomingOrder.floor, true)  //BT_Cab, 2 eller CAB??
+					}
+					if handleIncomingOrder.Class == HALL_UP {
+						SetButtonLamp(0, handleIncomingOrder.floor, true)  //BT_hallup, 0 eller HALL_UP??
+					}
+					if handleIncomingOrder.Class == HALL_DOWN {	 //legg inn at dette skal skje på alle heisene 
+						SetButtonLamp(1, handleIncomingOrder.floor, true)  //BT_hallup/down, 0,1 eller HALL_UP/DOWN??
+					}
+				}	
+				if handleIncomingOrder.Completed == true {
+					for f := 0; f < 3; f++ {
+						SetButtonLamp(f, handleIncomingOrder.floor, false) 
+					}
+				}
+			}
+		case floorIndicator:
+			floor = getFloor()
+			SetFloorIndicator(floor)
+		}
+		case openDoor: 	// sett åpen dør knapp 
+			//settes viss motordir = stop og timeren ikke har rent ut 
+
 	}
 }
