cf90c3828bd73eb144eb4e230068ad3832d4ccef
Kristian <krbrudeli@gmail.com>
2020-03-04 15:40:15 +0100

    Added delegator boilerplate

--- src/delegator/delegator.go
+++ src/delegator/delegator.go
@@ -4,11 +4,76 @@ import (
 	"Go-heisen/src/order"
 )
 
+type CostRequest struct {
+	Order order.Order
+}
+
+type CostResponse struct {
+	OrderID string
+	Cost    int
+}
+
+type orderDelegation struct {
+	o order.Order
+	costs map[string] int
+	disallowedRecipent string
+}
+
+func makeOrderDelegation(o order, disallowed)
+
 func Delegator(
-	toDelegator chan order.Order,
+	toDelegate chan order.Order,
+	toRedelegate chan order.Order,
 	toTransmitter chan order.Order,
+	costRequestTx chan CostRequest,
+	costResponseRx chan CostResponse,
 ) {
+	currentlyRedelegating := make(map[string]bool)
+	/*
+		initialize delegations
+
+		toDelegate:
+			set order in currently delegating
+			set costReqTimeout
+
+		toRedelegate:
+			if not currently redelegating
+				set redelegation
+				send to toDelegate
+
+		costReq:
+			check cost
+			send costresponse
+
+		costResponse:
+			if currently delegating order
+				if responder not in disallowed recipents for order
+					update costs
+					if enough costs
+						set recipent, send to processor
+						remove from currently delegating
+
+		costTimeOut
+			if currently delegating
+				delegate based on current costs
+
+	*/
+
+	delegations := make(map[string] orderDelegation)
+
 	for {
-		select {}
+		select {
+		case orderToDelegate := <-toDelegate:
+			id := orderToDelegate.OrderID
+			if _, currentlyDelegating := delegations[id]; !currentlyDelegating {
+				delegations[id] = 
+			}
+
+		case orderToRedelegate := <-toRedelegate:
+			
+
+		case costResponse := <-costResponseRx
+			
+		}
 	}
 }
