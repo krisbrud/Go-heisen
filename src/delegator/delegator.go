1a6619635db8b0e5cbf97e040a575742b9e1e7a2
Kristian <krbrudeli@gmail.com>
2020-03-13 11:52:50 +0100

    Before delegator refactor

--- src/delegator/delegator.go
+++ src/delegator/delegator.go
@@ -2,6 +2,7 @@ package delegator
 
 import (
 	"Go-heisen/src/order"
+	"fmt"
 )
 
 type CostRequest struct {
@@ -9,29 +10,29 @@ type CostRequest struct {
 }
 
 type CostResponse struct {
-	OrderID 	string
-	ResponderID	string
-	Cost    	int
+	OrderID     int
+	ResponderID string
+	Cost        int
 }
 
 type orderDelegation struct {
-	o order.Order
-	costs map[string] int
+	o                  order.Order
+	costs              map[string]int
 	disallowedRecipent string
 }
 
-func makeOrderDelegation(o order) {
+func makeOrderDelegation(o order.Order) orderDelegation {
 	return orderDelegation{
-		o: o,
-		costs: [],
-		disallowedRecipent: ""
+		o:                  o,
+		costs:              make(map[string]int),
+		disallowedRecipent: "",
 	}
 }
 
-func makeOrderRedelegation(o order, disallowed string) {
+func makeOrderRedelegation(o order.Order, disallowed string) orderDelegation {
 	return orderDelegation{
-		o: o,
-		costs: [],
+		o:                  o,
+		costs:              make(map[string]int),
 		disallowedRecipent: disallowed,
 	}
 }
@@ -40,8 +41,10 @@ func Delegator(
 	toDelegate chan order.Order,
 	toRedelegate chan order.Order,
 	toTransmitter chan order.Order,
+	toProcessor chan order.Order,
 	costRequestTx chan CostRequest,
 	costResponseRx chan CostResponse,
+	peerUpdates chan []string,
 ) {
 	currentlyRedelegating := make(map[string]bool)
 	/*
@@ -70,11 +73,13 @@ func Delegator(
 
 		costTimeOut
 			if currently delegating
-				delegate based on current costs
+
 
 	*/
 
-	delegations := make(map[string] orderDelegation)
+	delegations := make(map[int]orderDelegation)
+	redelegations := make(map[int]bool)
+	peers := make([]string, 0)
 
 	for {
 		select {
@@ -85,11 +90,15 @@ func Delegator(
 			}
 
 		case orderToRedelegate := <-toRedelegate:
-			id := orderToDelegate.OrderID
-			disallowedRecipent := orderToRedelegate.RecipentID;
-			if _, currentlyDelegating := delegations[id]; !currentlyDelegating {
-				delegations[id] = makeOrderRedelegation(orderToDelegate, disallowedRecipent)
+			// Redelegate the order if it isn't redelegated already
+			if _, alreadyRedelegating := redelegations[orderToRedelegate.OrderID]; alreadyRedelegating {
+				break
 			}
+
+			disallowedRecipent := orderToRedelegate.RecipentID
+			orderToRedelegate.OrderID = order.GetRandomID()
+
+			delegations[orderToRedelegate.OrderID] = makeOrderRedelegation(orderToRedelegate, disallowedRecipent)
 
 		case costResponse := <-costResponseRx:
 			orderID := costResponse.OrderID
@@ -99,13 +108,12 @@ func Delegator(
 			delegation, currentlyDelegating := delegations[orderID]
 
 			if !currentlyDelegating {
-				break;
+				break
 			}
-					
 			if responderID == delegation.disallowedRecipent {
 				break
 			}
-			
+
 			// Everything seems fine, update the cost if it is not present
 			// or worse than the one there
 			if oldCost, costExists := delegation.costs[responderID]; costExists {
@@ -116,9 +124,58 @@ func Delegator(
 				delegations[orderID].costs[responderID] = cost
 			}
 
-			// Check if there are enough costs
+			if !enoughCosts(delegation, peers) {
+				break
+			}
 
-						
+			// Delegate the order to the elevator with the lowest cost which is still in peers
+			bestElevatorID, err := lowestCost(delegation, peers)
+
+			if err != nil {
+				fmt.Printf(err.Error())
+				break
+			}
+
+			delegation.o.RecipentID = bestElevatorID
+
+			if !delegation.o.IsValid() {
+				fmt.Println("Order to delegate was not valid!")
+				break
+			}
+
+		case peerUpdate := <-peerUpdates:
+			peers = peerUpdate
 		}
 	}
+}
+
+func lowestCost(delegation orderDelegation, peers []string) (string, error) {
+	bestElevatorID := ""
+	bestCost := 10000 // TODO: Refactor
+
+	for elevatorID, cost := range delegation.costs {
+		if elevatorID != delegation.disallowedRecipent && cost < bestCost {
+			bestCost = cost
+			bestElevatorID = elevatorID
+		}
+	}
+
+	if bestElevatorID == "" {
+		err := fmt.Errorf("Did not any valid elevator to delegate to! Delegation %#v\n Peers: %#v\n", delegation, peers)
+		return "", err
+	}
+
+	return bestElevatorID, nil
+}
+
+func enoughCosts(delegation orderDelegation, peers []string) bool {
+	numValidCosts := len(delegation.costs) // No need to check if disallowed, they are not added to the map
+	numNeededCosts := len(peers)
+
+	if delegation.disallowedRecipent != "" {
+		// Redelegating order, one elevator disallowed
+		numNeededCosts--
+	}
+
+	return numValidCosts >= numNeededCosts
 }
