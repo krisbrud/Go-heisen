581cf44a6348dbb6ab35d40b85a0be96f46398ac
Kristian <krbrudeli@gmail.com>
2020-03-16 13:27:59 +0100

    Lightmanager refactor first draft finished

--- src/controller/controller.go
+++ src/controller/controller.go
@@ -1,87 +1,81 @@
 package controller
 
 import (
-	"time"
+	"Go-heisen/src/elevatorio"
+	"Go-heisen/src/elevatorstate"
+	"Go-heisen/src/order"
+	"fmt"
 )
 
 func Controller(
-	toButtonPushHandler chan order.Order, 
-	toArrivedFloorHandler chan elevatorstate.ElevatorState, 
-	toDelegator chan elevatorstate.ElevatorState, 
-	readState chan elevatorstate.ElevatorState,
-	readQueue chan order.Order,
+	toButtonPushHandler chan order.Order,
+	toArrivedFloorHandler chan elevatorstate.ElevatorState,
+	toDelegator chan elevatorstate.ElevatorState,
+	incomingOrders chan order.Order,
+	// readState chan elevatorstate.ElevatorState,
+	// readQueue chan order.Order,
 ) {
-	
-	var state elevatorstate.ElevatorState  
-	var floor int
-	var destination int 
+	// Initialize driver
+	numFloors := 4 // TODO: Refactor
+	elevatorio.Init("localhost:15657", numFloors)
+
+	drv_buttons := make(chan elevatorio.ButtonEvent)
+	drv_floors := make(chan int)
+	drv_stop := make(chan bool)
+
+	go elevatorio.PollButtons(drv_buttons)
+	go elevatorio.PollFloorSensor(drv_floors)
+	go elevatorio.PollStopButton(drv_stop)
+
+	// Initialize belief state
+	var state elevatorstate.ElevatorState
+	var destination int
+
+	// Initialize LightManager
+	toLightManager := make(chan order.Order)
+	go lightManager(toLightManager)
 
 	for {
 		select {
-			
-		case buttonPushed := <- drv_buttons:
-			go func() { toButtonPushHandler <- buttonPushed }() // må sende dette til button pushed handler 
+		case buttonPushed := <-drv_buttons:
+			// TODO: Make order based on pushed button
+			// go func() { toButtonPushHandler <- buttonPushed }() // må sende dette til button pushed handler
 
 		case elevatorStateChanged := <-readState:
-			if elevatorStateChanged != state{
-				go func() { toDelegator <- elevatorStateChanged }() // må lese elevator state og sende den til delegatoren 
+			if elevatorStateChanged != state {
 				state := elevatorStateChanged
+				go func() { toDelegator <- elevatorStateChanged }() // må lese elevator state og sende den til delegatoren
 			}
-			
-		case arrivedAtFloor := <- drv_floors: 
-			if arrivedFloor == destination{
-				elevio.SetMotorDirection(elevio.MD_Stop)
-				go func() { toArrivedFloorHandler <- drv_floors }() // må sende beskjed til arrived floor handler 
+
+		case arrivedFloor := <-drv_floors:
+			if arrivedFloor == destination {
+				elevatorio.SetMotorDirection(elevatorio.MD_Stop)
+				// TODO: send state instead
+				// go func() { toArrivedFloorHandler <- drv_floors }() // må sende beskjed til arrived floor handler
 			}
-			
-		case orderToExecute := <- reaadQueue:
-			floor = elevio.GetFloor()
-			destination = orderToExecute.Floor
-			if floor < destination {
-				elevio.SetMotorDirection(elevio.MD_Up)
-			} else if floor > destination {
-				elevio.SetMotorDirection(elevio.MD_Down)
-			}
+
+			// case orderToExecute := <- readQueue:
+			// 	destination = orderToExecute.Floor
+			// 	if floor < destination {
+			// 		elevatorio.SetMotorDirection(elevatorio.MD_Up)
+			// 	} else if floor > destination {
+			// 		elevatorio.SetMotorDirection(elevatorio.MD_Down)
+			// 	}
 		}
 	}
 }
 
-
-func lightManager(
-	incomingOrders chan order.Order,
-	toElevatorNetwork chan order.Order
-) {
-	
-	for {
-		select {
-		case handleIncomingOrder := <- incomingOrders: 
-			go func() {
-				if handleIncomingOrder.Completed == false {
-					if handleIncomingOrder.Class == CAB && handleIncomingOrder.isMine(){  
-						SetButtonLamp(2, handleIncomingOrder.floor, true)  //BT_Cab, 2 eller CAB??
-					}
-					if handleIncomingOrder.Class == HALL_UP {
-						SetButtonLamp(0, handleIncomingOrder.floor, true)  //BT_hallup, 0 eller HALL_UP??
-					}
-					if handleIncomingOrder.Class == HALL_DOWN {	 
-						SetButtonLamp(1, handleIncomingOrder.floor, true)  //BT_hallup/down, 0,1 eller HALL_DOWN??
-					}
-				}	
-				if handleIncomingOrder.Completed == true {
-					for f := 0; f < 3; f++ {
-						SetButtonLamp(f, handleIncomingOrder.floor, false)
-						if handleIncomingOrder.isMine() {
-							SetDoorOpenLamp(true)
-							time.Sleep(3* time.Second) //meeeeget usikker på denne
-						} 
-						SetDoorOpenLamp(false)
-					}
-				}
-			}
-		case floorIndicator:
-			floor = getFloor()
-			SetFloorIndicator(floor)
-		}
-
+func setOrderLights(incomingOrder order.Order) {
+	if !incomingOrder.IsValid() {
+		fmt.Printf("Trying to set lights of invalid order! Order: %#v", incomingOrder)
+		return
 	}
+
+	if incomingOrder.IsFromCab() && !incomingOrder.IsMine() {
+		// Cab call but not mine, don't change any lights
+		return
+	}
+
+	// All is good, set the lights
+	elevatorio.SetButtonLamp(0, incomingOrder.Floor, incomingOrder.Completed) //BT_hallup, 0 eller HALL_UP??
 }
